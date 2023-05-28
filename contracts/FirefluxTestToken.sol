189a7ccfc8b8f4165c5b7c3cdeeededc362647a9
pascal <pascal>
2023-05-28 15:52:32 -0400

    chore: update contracts to support verification on conflux

--- contracts/FirefluxTestToken.sol
+++ contracts/FirefluxTestToken.sol
@@ -5,7 +5,7 @@ pragma solidity >= 0.8.0;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FirefluxTestToken is ERC20 {
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+    constructor() ERC20("FirefluxTestToken", "FTT") {}
 
     function mint(address _account, uint256 _amount) external {
         _mint(_account, _amount);

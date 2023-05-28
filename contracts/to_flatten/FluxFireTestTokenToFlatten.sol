ca60d824a3291bfd7166854dd77e87ee14695415
pascal <pascal>
2023-05-28 16:34:23 -0400

    chore: redo proxy setup + prepare files for flattening

--- contracts/to_flatten/FluxFireTestTokenToFlatten.sol
+++ contracts/to_flatten/FluxFireTestTokenToFlatten.sol
@@ -4,8 +4,8 @@ pragma solidity >= 0.8.0;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract FirefluxTestToken is ERC20 {
-    constructor() ERC20("FirefluxTestToken", "FTT") {}
+contract FluxFireTestTokenFlat is ERC20 {
+    constructor() ERC20("FluxFireTestToken", "FTT") {}
 
     function mint(address _account, uint256 _amount) external {
         _mint(_account, _amount);
